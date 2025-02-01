71d6f98d47f8678190ad0b000625c0cf9da19788
Propaganistas <Propaganistas@users.noreply.github.com>
2025-02-01 20:07:08 +0100

    bubble exception instead of firing NotificationFailed

--- src/Notifications/SmsChannel.php
+++ src/Notifications/SmsChannel.php
@@ -2,9 +2,6 @@
 
 namespace Propaganistas\LaravelSms\Notifications;
 
-use Exception;
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Notifications\Events\NotificationFailed;
 use Illuminate\Notifications\Notification;
 use Propaganistas\LaravelSms\SmsManager;
 
@@ -12,7 +9,6 @@ class SmsChannel
 {
     public function __construct(
         protected SmsManager $smsManager,
-        protected ?Dispatcher $dispatcher = null
     ) {
     }
 
@@ -22,24 +18,9 @@ class SmsChannel
 
         $recipient = $notifiable->routeNotificationFor('sms');
 
-        try {
-            $this->smsManager
-                ->to($recipient)
-                ->withNotifiable($notifiable)
-                ->send($message);
-        } catch (Exception $e) {
-            $this->sendNotificationFailed($notifiable, $notification, $e);
-        }
-    }
-
-    protected function sendNotificationFailed($notifiable, Notification $notification, Exception $e)
-    {
-        if ($this->dispatcher !== null) {
-            $this->dispatcher->dispatch(
-                new NotificationFailed($notifiable, $notification, 'sms', [
-                    'message' => $e->getMessage(),
-                ])
-            );
-        }
+        $this->smsManager
+            ->to($recipient)
+            ->withNotifiable($notifiable)
+            ->send($message);
     }
 }
--- tests/Notifications/SmsChannelTest.php
+++ tests/Notifications/SmsChannelTest.php
@@ -2,17 +2,13 @@
 
 namespace Propaganistas\LaravelSms\Tests\Notifications;
 
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Notifications\Events\NotificationFailed;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Event;
 use Mockery;
 use PHPUnit\Framework\Attributes\Test;
 use Propaganistas\LaravelSms\Notifications\SmsChannel;
 use Propaganistas\LaravelSms\SmsManager;
 use Propaganistas\LaravelSms\Tests\TestCase;
-use RuntimeException;
 
 class SmsChannelTest extends TestCase
 {
@@ -20,7 +16,6 @@ class SmsChannelTest extends TestCase
     public function it_constructs()
     {
         $this->assertInstanceOf(SmsChannel::class, new SmsChannel($this->app[SmsManager::class]));
-        $this->assertInstanceOf(SmsChannel::class, new SmsChannel($this->app[SmsManager::class], $this->app[Dispatcher::class]));
     }
 
     #[Test]
@@ -36,30 +31,6 @@ class SmsChannelTest extends TestCase
 
         $channel = new SmsChannel($mock);
         $channel->send($notifiable, $notification);
-    }
-
-    #[Test]
-    public function it_dispatches_notification_failed_event_on_exception()
-    {
-        Event::fake();
-
-        $notifiable = new StubNotifiable;
-        $notification = new StubNotification;
-
-        $mock = Mockery::mock(SmsManager::class);
-        $mock->shouldReceive('to')->once()->withArgs(['0123'])->andReturnSelf();
-        $mock->shouldReceive('withNotifiable')->once()->withArgs([$notifiable])->andReturnSelf();
-        $mock->shouldReceive('send')->once()->andThrow(new RuntimeException('Oh no'));
-
-        $channel = new SmsChannel($mock, Event::getFacadeRoot());
-        $channel->send($notifiable, $notification);
-
-        Event::assertDispatched(NotificationFailed::class, function (NotificationFailed $event) use ($notifiable, $notification) {
-            return $event->notifiable === $notifiable
-                && $event->notification === $notification
-                && $event->channel === 'sms'
-                && $event->data === ['message' => 'Oh no'];
-        });
     }
 }
 
